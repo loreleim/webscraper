8bf2c86c08163c2f73719b550e245293ec9649a1
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-05-13 21:42:05 -1000

    attempting an image scrape

--- puppeteer.js
+++ puppeteer.js
@@ -1,8 +1,15 @@
 const puppeteer = require('puppeteer');
 
-const scrapeImages = async(passedUrl) => {
+const scraper = async(passedUrl) => {
   const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
-
   await page.goto(passedUrl);
+
+  const imgs = await page.$$eval('img.xyz[src]', imgs => imgs.map(img => img.getAttribute('src')));
+
+  console.log({imgs});
+
+  browser.close();
 }
+
+scraper('http://www2.hawaii.edu/~eherring/hawnprop/dod-visc.htm');
